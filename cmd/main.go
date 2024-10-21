a031fda3f621ed4bab6aaaaba33a2cdb85154459
John <johneliud4@gmail.com>
2024-10-21 23:44:34 +0300

    chore: setup handler function that serves static files and index page of the website.

---
+++ cmd/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/johneliud/my-portfolio/controllers"
+)
+
+func main() {
+	if len(os.Args) < 1 || len(os.Args) > 2 {
+		fmt.Println("Usage: 'go run .' OR 'go run . [PORT]'")
+		return
+	}
+
+	http.HandleFunc("/static/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/static/" {
+			http.Error(w, "Access Forbidden", http.StatusForbidden)
+			return
+		}
+
+		http.StripPrefix("/static/", http.FileServer(http.Dir("static"))).ServeHTTP(w, r)
+	})
+
+	http.HandleFunc("/", controllers.IndexHandler)
+
+	var port string
+
+	switch len(os.Args) {
+	case 1:
+		port = ":8080"
+	case 2:
+		port = os.Args[1]
+		portNum, err := strconv.Atoi(port[1:])
+		if err != nil {
+			fmt.Printf("Failed converting %v to an int: %v", port[1:], err)
+			return
+		}
+
+		if portNum < 1024 || portNum > 65535 {
+			fmt.Println("Invalid port number. Must be in the range of 1024-65535")
+			return
+		}
+		port = ":" + strconv.Itoa(portNum)
+	}
+	log.Printf("Server running on http://localhost%v\n", port)
+	http.ListenAndServe(port, nil)
+}
