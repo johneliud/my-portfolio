1e412d38b7a854d78bf4f01e669bd9673c13d689
John <johneliud4@gmail.com>
2024-10-27 10:28:28 +0300

    fix: theme implementation to save user preference on local storage to prevent switching if browser is refreshed.

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,30 @@
 const modeToggler = document.querySelectorAll('.change-mode');
 
-const toggleTheme = () => {
-  document.body.classList.toggle('dark-theme');
-  modeToggler.forEach((toggler) => {
-    toggler.classList.toggle('dark-theme');
-  });
+// Apply theme based on the value in localStorage
+const applyTheme = (theme) => {
+  if (theme === 'dark') {
+    document.body.classList.add('dark-theme');
+    modeToggler.forEach((toggler) => {
+      toggler.classList.add('dark-theme');
+    });
+  } else {
+    document.body.classList.remove('dark-theme');
+    modeToggler.forEach((toggler) => {
+      toggler.classList.remove('dark-theme');
+    });
+  }
 };
+
+const toggleTheme = () => {
+  const currentTheme = document.body.classList.contains('dark-theme')
+    ? 'dark'
+    : 'light';
+  const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+  applyTheme(newTheme);
+  localStorage.setItem('theme', newTheme);
+};
+const savedTheme = localStorage.getItem('theme') || 'light';
+applyTheme(savedTheme);
 
 modeToggler.forEach((toggler) => {
   toggler.addEventListener('click', toggleTheme);
