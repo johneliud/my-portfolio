f26d6794095d9cf1fcc25598f1ed05ebfa4da6e4
John <johneliud4@gmail.com>
2024-10-29 22:31:13 +0300

    remove: data structure used to comprehend the contact form to models package.

--- controllers/form-handler.go
+++ controllers/form-handler.go
@@ -2,51 +2,14 @@ package controllers
 
 import (
 	"encoding/json"
-	"errors"
 	"html/template"
 	"log"
 	"net/http"
 	"path/filepath"
-	"regexp"
+
+	"github.com/johneliud/my-portfolio/models"
+	"github.com/johneliud/my-portfolio/utils"
 )
-
-type ContactForm struct {
-	Name    string
-	Email   string
-	Subject string
-	Message string
-}
-
-type Response struct {
-	Success bool
-	Message string
-	Error   string
-}
-
-// Validation patterns
-var (
-	nameRegex  = regexp.MustCompile(`^[a-zA-Z\s]{3,50}$`)
-	emailRegex = regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
-)
-
-func (cf *ContactForm) Validate() error {
-	if !nameRegex.MatchString(cf.Name) {
-		return errors.New("invalid name format: must be 3-50 characters long and contain only letters and spaces")
-	}
-
-	if !emailRegex.MatchString(cf.Email) {
-		return errors.New("invalid email format")
-	}
-
-	if len(cf.Subject) > 100 {
-		return errors.New("subject must be less than 100 characters")
-	}
-
-	if len(cf.Message) < 10 || len(cf.Message) > 1000 {
-		return errors.New("message must be between 10 and 1000 characters")
-	}
-	return nil
-}
 
 func FormHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
@@ -66,7 +29,7 @@ func FormHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	case http.MethodPost:
 		// Process form submission
-		var form ContactForm
+		var form models.ContactForm
 
 		if err := json.NewDecoder(r.Body).Decode(&form); err != nil {
 			InternalServerErrorHandler(w, r)
@@ -80,13 +43,13 @@ func FormHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		if err := SendEmail(form); err != nil {
+		if err := utils.SendEmail(form); err != nil {
 			InternalServerErrorHandler(w, r)
 			log.Printf("Failed to send email: %v\n", err)
 			return
 		}
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(Response{
+		json.NewEncoder(w).Encode(models.Response{
 			Success: true,
 			Message: "Message sent successfully!",
 		})
