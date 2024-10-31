450a6a667bd47961631de6d340ded207545c3230
John <johneliud4@gmail.com>
2024-11-01 00:22:01 +0300

    add: data structures used to handle data on the site.

--- models/models.go
+++ models/models.go
@@ -1,10 +1,5 @@
 package models
 
-import (
-	"errors"
-	"regexp"
-)
-
 type ContactForm struct {
 	Name    string
 	Email   string
@@ -18,27 +13,12 @@ type Response struct {
 	Error   string
 }
 
-// Validation patterns
-var (
-	nameRegex  = regexp.MustCompile(`^[a-zA-Z\s]{3,50}$`)
-	emailRegex = regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
-)
+type ErrorPage struct {
+	StatusCode int
+	Message    string
+}
 
-func (cf *ContactForm) Validate() error {
-	if !nameRegex.MatchString(cf.Name) {
-		return errors.New("invalid name format: must be 3-50 characters long and contain only letters and spaces")
-	}
-
-	if !emailRegex.MatchString(cf.Email) {
-		return errors.New("invalid email format")
-	}
-
-	if len(cf.Subject) > 100 {
-		return errors.New("subject must be less than 100 characters")
-	}
-
-	if len(cf.Message) < 10 || len(cf.Message) > 1000 {
-		return errors.New("message must be between 10 and 1000 characters")
-	}
-	return nil
+type ErrorResponse struct {
+	Success bool   `json:"success"`
+	Error   string `json:"error"`
 }
