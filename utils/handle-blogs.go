7ffbc884a3e48fb9e0e7abc8c183240492687bd5
John <johneliud4@gmail.com>
2024-11-01 01:59:00 +0300

    add: function LoadPosts that reads from the posts to generate a preview for the blog.

--- utils/handle-blogs.go
+++ utils/handle-blogs.go
@@ -2,7 +2,12 @@ package utils
 
 import (
 	"fmt"
+	"io/fs"
 	"os"
+	"path/filepath"
+	"sort"
+
+	"github.com/johneliud/my-portfolio/models"
 )
 
 type BlogStore struct {
@@ -14,4 +19,47 @@ func NewBlogStore(postsDirectory string) (*BlogStore, error) {
 		return nil, fmt.Errorf("failed to create posts directory: %w", err)
 	}
 	return &BlogStore{postsDir: postsDirectory}, nil
+}
+
+// Reads all blog posts for preview
+func (bs *BlogStore) LoadPosts() ([]models.BlogPost, error) {
+	var posts []models.BlogPost
+
+	err := filepath.WalkDir(bs.postsDir, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if filepath.Ext(path) != ".json" {
+			return nil
+		}
+
+		post, err := bs.loadPost(path)
+		if err != nil {
+			return fmt.Errorf("error loading post %s: %w", path, err)
+		}
+
+		// Create blog preview
+		previewPost := models.BlogPost{
+			ID:          post.ID,
+			Title:       post.Title,
+			Summary:     post.Summary,
+			Date:        post.Date,
+			ReadingTime: post.ReadingTime,
+			Tags:        post.Tags,
+		}
+
+		posts = append(posts, previewPost)
+		return nil
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("error walking posts directory: %w", err)
+	}
+
+	// Sort posts by date, newest first
+	sort.Slice(posts, func(i, j int) bool {
+		return posts[i].Date.After(posts[j].Date)
+	})
+	return posts, nil
 }
