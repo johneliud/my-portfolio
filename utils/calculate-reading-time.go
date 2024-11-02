ea8ca8117fee87209159e375b0ccc9a99fe10552
John <johneliud4@gmail.com>
2024-11-02 15:41:38 +0300

    refactor: function to read from a file and determine the length and reading time.

--- utils/calculate-reading-time.go
+++ utils/calculate-reading-time.go
@@ -1,9 +1,40 @@
 package utils
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func calculateWordCount() int {
+	articleFile, err := os.Open("articles/file-integrity.txt")
+	if err != nil {
+		log.Println("Error opening file:", err)
+		return 0
+	}
+	defer articleFile.Close()
+
+	wordCount := 0
+	scanner := bufio.NewScanner(articleFile)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		words := strings.Fields(line)
+		wordCount += len(words)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+		return 0
+	}
+
+	return wordCount
+}
 
 func CalculateReadingTime(content string) int {
 	// Assuming the average speed is 200 words per minute
-	wordCount := len(strings.Fields(content))
+	wordCount := calculateWordCount()
 	return (wordCount + 199) / 200
 }
